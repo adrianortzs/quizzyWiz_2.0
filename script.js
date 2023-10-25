8c162edcbbed1d0f2bd076587fef5219a5a1d8ae
adrianortzs <adrianortizsuarez@icloud.com>
2023-10-25 17:07:32 +0200

    rematando cosas

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // BOTON EMPEZAR
-  const startButton = document.getElementById("startButton");
+const firebaseConfig = {
+  apiKey: "AIzaSyDlsqX9gEU6ToguhB0yuc1kKTtU93kcBMA",
+  authDomain: "quizii-84a42.firebaseapp.com",
+  projectId: "quizii-84a42",
+  storageBucket: "quizii-84a42.appspot.com",
+  messagingSenderId: "201517071704",
+  appId: "1:201517071704:web:f18386349818ced992f92d",
+};
 
-  // BOTON SIGUIENTE PREGUNTA
+firebase.initializeApp(firebaseConfig);
 
-  function showQuestion(questions, index) {
-  let section = document.querySelector("#question_quiz");
-  let question = questions[index];
-
-  if (question) {
-    let arrTemplateString = `
-      <h1 class="pregunta">${question.question}</h1>
-      <label><input type="radio" value="${question.correctAnswer}" name="res">${question.correctAnswer}</label>
-      <label><input type="radio" value="${question.incorrectAnswers[0]}" name="res">${question.incorrectAnswers[0]}</label>
-      <label><input type="radio" value="${question.incorrectAnswers[1]}" name="res">${question.incorrectAnswers[1]}</label>
-      <label><input type="radio" value="${question.incorrectAnswers[2]}" name="res">${question.incorrectAnswers[2]}</label>
-    `;
-    section.innerHTML = arrTemplateString;
-  } else {
-    section.innerHTML = "No hay más preguntas.";
-    }
-  }
-})
-
-// LLAMAR AL EVENTO DE LOS BOTONES EMPEZAR Y SIGUIENTE
+const db = firebase.firestore();
 
 document.addEventListener("DOMContentLoaded", function () {
-  //BOTON EMPEZAR
   const startButton = document.getElementById("startButton");
-
-  //BOTON SIGUIENTE PREGUNTA
-
   const nextButton = document.getElementById("nextButton");
 
-  // REGISTRO DE LA PREGUNTA
   let questionIndex = 0;
-
   let score = 0;
-
-  // Función INFORMACIÓN API y GUARDAR EN LOCALSTORE
+  let correctResponses = [];
 
   async function getQuestionsApi() {
     try {
@@ -54,218 +33,112 @@ document.addEventListener("DOMContentLoaded", function () {
         incorrectAnswers: question.incorrect_answers,
       }));
 
-      // Guarda preguntas en el LocalStorage
       localStorage.setItem("questionsData", JSON.stringify(getInfo));
-
-      localStorage.setItem(
-        "answerCorrect",
-        JSON.stringify(getInfo.correctAnswer)
-      );
-      localStorage.setItem(
-        "answerFalse",
-        JSON.stringify(getInfo.incorrectAnswers)
-      );
-
-      // Mostrar la primera pregunta
       showQuestion(getInfo, questionIndex);
     } catch (error) {
       console.log(`Hubo un error: ${error}`);
     }
   }
-})
 
-  // OCULTAR BOTÓN EMPEZAR, TITULO Y MENSAJE BIENVENIDA"
-startButton.addEventListener("click", function () {
-  getQuestionsApi();
-
-  // Ocultar botón start game
-  startButton.style.display = "none";
-
-  // OCULTAR MENSAJE BIENVENIDA
-  const welcomeContainer = document.querySelector("#welcomeContainer");
-  welcomeContainer.style.display = "none";
-
-  // MOSTRAR preguntas y respuestas
-  const questionContainer = document.querySelector("#playQuiz");
-  questionContainer.style.display = "block";
-
-  // MOSTRAR BOTÓN NEXT
-  nextButton.style.display = "block";
-  });
-
-  // VALIDACIÓN
-nextButton.addEventListener("click", function () {
-  const typeAnswer = document.querySelectorAll('input[type="radio"]:checked');
-  let correctResponses = [];
-  let incorrectResponses = [];
-
-  if (typeAnswer.length === 1) {
-    const selectedInput = typeAnswer[0];
-    const answer = selectedInput.value;
-
-    const questionsData = getQuestionsFromLocalStorage();
-    const currentQuestion = questionsData[questionIndex];
-
-    if (answer === currentQuestion.correctAnswer) { 
-      selectedInput.parentNode.classList.add("correct");
-      correctResponses.push(answer);
-      score += 1;
-    } else {
-      selectedInput.parentNode.classList.add("incorrect");
-      incorrectResponses.push(answer);
-      correctResponses.push(currentQuestion);
-      score += 1;
-      } 
-
-    console.log(score);
-
-    questionIndex++;
-      
-
-    if (questionIndex < questionsData.length) {
-
-    // Ocultar el botón de inicio
+  startButton.addEventListener("click", function () {
+    getQuestionsApi();
     startButton.style.display = "none";
-
-    // Ocultar el mensaje de bienvenida
     const welcomeContainer = document.querySelector("#welcomeContainer");
     welcomeContainer.style.display = "none";
+    const questionContainer = document.querySelector("#playQuiz");
+    questionContainer.style.display = "block";
 
-    // Mostrar el botón "Siguiente Pregunta"
     nextButton.style.display = "block";
-    }
+  });
+
+  function processUserDataAndShowChart() {
+    // ... (código para procesar usuarios y mostrar la gráfica)
   }
-})
 
-// FUNCION PARA MOSTAR LAS PREGUNTAS
-nextButton.addEventListener("click", function () {
-  // COMPROBAR QUE ALGUNA OPCION HA SIDO SELECCIONADA
-  const typeAnswer = document.querySelectorAll('input[type="radio"]');
+  nextButton.addEventListener("click", async function () {
+    const typeAnswer = document.querySelectorAll('input[type="radio"]:checked');
 
-  let answerSelected = false;
-  for (let i = 0; i < typeAnswer.length; i++) {
-    if (typeAnswer[i].checked) {
-      answerSelected = true;
-      break;
-    }
-  } // UNA VEZ COMPROBADO Y QUE SEA TRUE REALIZA EL SIGUIENTE CONDICIONAL
-  if (answerSelected) {
-    questionIndex++;
+    if (typeAnswer.length === 1) {
+      const selectedInput = typeAnswer[0];
+      const answer = selectedInput.value;
 
-    const questionsData = getQuestionsFromLocalStorage();
+      const currentQuestion = getQuestionsFromLocalStorage()[questionIndex];
+      const totalQuestions = getQuestionsFromLocalStorage().length;
 
-    // CONDICIONAL PARA COMPROBAR SI HAY MÁS PREGUNTAS ALMACENADAS
-    if (questionsData && questionIndex < questionsData.length) {
-
-      showQuestion(questionsData, questionIndex);
-    } else {
-      const section = document.getElementById("question_quiz");
-      section.innerHTML = "¡HAS TERMINADO LAS PREGUNTAS DE QUIZZYWIZ!";
-
-      // OCULTAMOS BOTÓN NEXT
-
-      nextButton.style.display = "none";
-
-      //Imprimir resultado en pantalla
-
-      if (questionIndex === questionsData.length) {
-        const totalQuestions = questionsData.length;
-        const percentage = (correctResponses.length / totalQuestions) * 100;
-        const resultQuiz = document.getElementById("results");
-        //revisar el lugar en donde se pinta porq se pinta
-        resultQuiz.innerHTML = `Has acertado el ${percentage}% de las preguntas`;
-      }
-        
-      //GRÁFICA
-
-      new Chartist.Bar('#chart2',  {
-      labels: [...user],             
-      series: [score]
-      })
-    }
+      if (answer === currentQuestion.correctAnswer) {
+        selectedInput.parentNode.classList.add("correct");
+        score += 1;
+        correctResponses.push(answer);
       } else {
-    // SI NO HA SIDO SELECCIONADA NINGUNA RESPUESTA
+        selectedInput.parentNode.classList.add("incorrect");
+      }
 
+      questionIndex++;
+
+      if (questionIndex < totalQuestions) {
+        showQuestion(getQuestionsFromLocalStorage(), questionIndex);
+      } else {
+        const section = document.getElementById("question_quiz");
+        section.innerHTML = "¡HAS TERMINADO LAS PREGUNTAS DE QUIZZYWIZ!";
+        nextButton.style.display = "none";
+
+        if (questionIndex === totalQuestions) {
+          const percentage = (score / totalQuestions) * 100;
+          const section1 = document.getElementById("results");
+          section1.innerHTML = `Has acertado el ${percentage}% de las preguntas`;
+
+          const user = firebase.auth().currentUser;
+
+          const game = {
+            user: user.l,
+            date: new Date(),
+            result: correctResponses.length,
+          };
+          await saveResults(game);
+        }
+        processUserDataAndShowChart();
+      }
+    } else {
       alert("Tienes que elegir alguna respuesta");
     }
-    if (questionIndex === questionsData.length) {
-      // NOTA RESULTADO
-      const totalQuestions = questionsData.length;
-      const percentage = (correctResponses.length / totalQuestions) * 100;
-      return percentage
-    }
-});
+  });
 
-
-// MOSTRAR LAS PREGUNTAS Y RESPUESTAS
-function showQuestion(questions, index) {
-  let section = document.getElementById("question_quiz");
-  let question = questions[index];
-  //Respuestas orden aleatorio
-  let mixedAnswers = [question.correctAnswer, ...question.incorrectAnswers];
-  mixedAnswers.sort(() => Math.random() - 0.5);
-}
-
-
-  // MOSTRAR LAS PREGUNTAS Y RESPUESTAS
   function showQuestion(questions, index) {
     let section = document.getElementById("question_quiz");
     let question = questions[index];
+    // Respuestas orden aleatorio
+    let mixedAnswers = [question.correctAnswer, ...question.incorrectAnswers];
+    mixedAnswers.sort(() => Math.random() - 0.5);
 
     let arrTemplateString = `
-
-    <h1 class="pregunta">${question.question}</h1>
-    <label><input type="radio" value="${mixedAnswers[0]}" name="res" required>${mixedAnswers[0]}</label>
-    <label><input type="radio" value="${mixedAnswers[1]}" name="res" required>${mixedAnswers[1]}</label>
-    <label><input type="radio" value="${mixedAnswers[2]}" name="res" required>${mixedAnswers[2]}</label>
-    <label><input type="radio" value="${mixedAnswers[3]}" name="res" required>${mixedAnswers[3]}</label>`
-
-  section.innerHTML = arrTemplateString;
-}
-
-function getQuestionsFromLocalStorage() {
-  let questionsData = localStorage.getItem("questionsData");
-  if (questionsData) {
-    return JSON.parse(questionsData);
+                <h3 class="pregunta">${question.question}</h3>
+                <label><input type="radio" value="${mixedAnswers[0]}" name="res" required>${mixedAnswers[0]}</label>
+                <label><input type="radio" value="${mixedAnswers[1]}" name="res" required>${mixedAnswers[1]}</label>
+                <label><input type="radio" value="${mixedAnswers[2]}" name="res" required>${mixedAnswers[2]}</label>
+                <label><input type="radio" value="${mixedAnswers[3]}" name="res" required>${mixedAnswers[3]}</label>
+            `;
+    section.innerHTML = arrTemplateString;
   }
-  return null;
-}
 
-function answersFromLocalStorage() {
-
-  let answers = localStorage.getItem("answers");
-  if (answers) {
-    return JSON.parse(answers);
-  let answerData = localStorage.getItem("answerCorrect");
-  if (answerData) {
-    return JSON.parse(answerCorrect);
+  function getQuestionsFromLocalStorage() {
+    let questionsData = localStorage.getItem("questionsData");
+    if (questionsData) {
+      return JSON.parse(questionsData);
+    }
+    return null;
   }
-  return null;
-}
-}
 
-function answersFromLocalStorage() {
-  let answerFal = localStorage.getItem("answerFalse");
-  if (answerFal) {
-    return JSON.parse(answerFal);
-  }
-  return null;
-}
+  // MIRAR AUTH
 
-// ***** CONSEGUIR DEJAR SELECIONADA UNA RESPUESTA CON EL COLOR ****** //
-
-// FUNCIONALIDAD LOGIN
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDlsqX9gEU6ToguhB0yuc1kKTtU93kcBMA",
-  authDomain: "quizii-84a42.firebaseapp.com",
-  projectId: "quizii-84a42",
-  storageBucket: "quizii-84a42.appspot.com",
-  messagingSenderId: "201517071704",
-  appId: "1:201517071704:web:f18386349818ced992f92d",
-};
-
-firebase.initializeApp(firebaseConfig);
+  const saveResults = async (result) => {
+    console.log("Valores que se van a guardar en Firestore:", result);
+    await db
+      .collection("quizII")
+      .add(result)
+      .then((docRef) => console.log("Document written with ID: ", docRef.id))
+      .catch((error) => console.error("Error adding document: ", error));
+  };
+});
 
 const createUser = (user) => {
   db.collection("quizII")
@@ -292,17 +165,12 @@ const signUpUser = (email, password) => {
     .then((userCredential) => {
       let user = userCredential.user;
       alert(`Se ha registrado ${user.email} en el sistema`);
-      createUser({
-        id: user.uid,
-        email: user.email,
-        message: "hola!",
-      });
+      createUser(game);
     })
     .catch((error) => {
       console.log("Error en el sistema" + error.message);
     });
 };
-
 
 const signInUser = (email, password) => {
   firebase
@@ -325,9 +193,35 @@ const signInUser = (email, password) => {
 
 document.getElementById("form1").addEventListener("submit", function (event) {
   event.preventDefault();
-  let email = event.target.elements.email.value;
-  let pass = event.target.elements.pass.value;
-  let pass2 = event.target.elements.pass2.value;
+
+  if (
+    /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+      event.target.elements.email.value
+    )
+  ) {
+    var email = event.target.elements.email.value;
+  } else {
+    alert("prueba con otra dirección email,formato no válido");
+  }
+
+  if (
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})/.test(
+      event.target.elements.pass.value
+    )
+  ) {
+    var pass = event.target.elements.pass.value;
+    //contraseña Caracola1!
+  } else {
+    alert("prueba con otra contraseña,formato no válido");
+  }
+
+  if (
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})/.test(
+      event.target.elements.pass2.value
+    )
+  ) {
+    var pass2 = event.target.elements.pass2.value;
+  }
 
   pass === pass2
     ? signUpUser(email, pass)
@@ -355,14 +249,6 @@ document.getElementById("form2").addEventListener("submit", function (event) {
   signInUser(email, pass);
 });
 
-
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    console.log(`Está en el sistema:${user.email} ${user.uid}`);
-  } else {
-    console.log("No hay usuarios en el sistema");
-  }
-});
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     console.log(`Está en el sistema:${user.email} ${user.uid}`);
